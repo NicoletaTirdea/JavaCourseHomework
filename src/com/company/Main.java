d6e68aaff1747fac60d9c6694e90c398a44f550c
nicoleta <tirdea.nicoleta@gmail.com>
2018-10-12 20:17:59 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        long l = 100292300043l;
+        float f = 23.2f;
+        System.out.println(l);
+
+        int i = (int) l;
+        System.out.println(i);
+    }
+
+}
